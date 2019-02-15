6d81b5ba36d47a06f091fa3643d810997a780fb2
Joker Jing <761878367@qq.com>
2019-02-15 20:45:15 +0800

    update

--- mybatis-generator/生成命令.bat
+++
@@ -1,5 +0,0 @@
-@echo on
-if not exist src\main\java  md src\main\java
-del src\main\java\* /q /f /s 
-java -jar mybatis-generator-core-1.3.5.jar -configfile generatorConfig.xml -overwrite
-echo. & pause
